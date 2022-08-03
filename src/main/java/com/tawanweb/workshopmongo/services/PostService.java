b9cb2d3ce5190ea16de513394a59313d2a6f266e
Tawan Silva <tawan.tls43@gmail.com>
2022-08-03 13:17:19 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tawanweb/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.tawanweb.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tawanweb.workshopmongo.domain.Post;
+import com.tawanweb.workshopmongo.repository.PostRepository;
+import com.tawanweb.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
